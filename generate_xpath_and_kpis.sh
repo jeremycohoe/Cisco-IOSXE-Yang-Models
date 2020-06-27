fbf181576054359ec736359af0054096ebc363d5
jeremycohoe <jcohoe@cisco.com>
2020-06-27 10:03:03 -0700

    Update generate_xpath_and_kpis.sh

--- generate_xpath_and_kpis.sh
+++ generate_xpath_and_kpis.sh
@@ -6,8 +6,10 @@ do
 echo
 echo
 echo > TREE/$i-tree.txt
+
 echo "KPI for model: $i" >> TREE/$i-tree.txt
 pyang -f tree $i >> TREE/$i-tree.txt
+
 echo "Xpaths for model: $i" >> TREE/$i-tree.txt
-pyang -f xpath $i >> TREE/$i-tree.txt
+pyang -f flatten --flatten-qualified-in-xpath $i >> TREE/$i-xpath.txt
 done
